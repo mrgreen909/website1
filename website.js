4c73b204ebf31be3d2ab235a611e6e4f4ef5a784
mrgreen909 <114020501+mrgreen909@users.noreply.github.com>
2022-09-20 22:13:27 +0200

    Update website.js

--- website.js
+++ website.js
@@ -6,11 +6,11 @@ let thumbnail = document.getElementsByClassName("thumbnail");
 let hero = document.getElementById("hero");
 
 let backgroundImg = new Array(
-    "/nummer2.png",
-    "/rot.png",
-    "/nummer3.png",
-    "/nummer4.png",
-    "/nummer5.png",
+    "nummer2.png",
+    "rot.png",
+    "nummer3.png",
+    "nummer4.png",
+    "nummer5.png",
 );
 let i = 0;
 next.onclick = function(){
