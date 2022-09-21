0318fa400b590cac220fb36abbefe046a562bf05
mrgreen909 <114020501+mrgreen909@users.noreply.github.com>
2022-09-21 19:51:16 +0200

    Update photo.js

--- photo.js
+++ photo.js
@@ -5,11 +5,11 @@ let thumbnail = document.getElementsByClassName("thumbnail");
 let hero = document.getElementById("hero");
 
 let backgroundImg = new Array(
-    "/bean.png",
-    "/unicorn2.png",
-    "/Design.png",
-    "/binarium2.png",
-    "/blume.png",
+    "bean.png",
+    "unicorn2.png",
+    "Design.png",
+    "binarium2.png",
+    "blume.png",
 );
 let i = 0;
 next.onclick = function(){
